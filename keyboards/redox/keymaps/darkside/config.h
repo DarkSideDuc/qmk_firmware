4a9a7ea3c89368de2c76f6ad895ad25dccdfe30f
DarkSideDuc <41841893+DarkSideDuc@users.noreply.github.com>
2018-07-28 15:35:58 -0700

    Update config.h

--- keyboards/redox/keymaps/darkside/config.h
+++ keyboards/redox/keymaps/darkside/config.h
@@ -23,17 +23,27 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Use I2C or Serial, not both */
 
 // #define USE_SERIAL
-#define USE_I2C
+// #define USE_I2C
+
+/* You need to use the serial communication since we 
+used a cable to carry the data signal for the LED strip */
+#define USE_SERIAL
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
+// #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
+/* Select hand configuration: you need to use as master 
+the hand that drives the LED strip */
+// #define MASTER_LEFT
+#define MASTER_RIGHT
+
+/* Put the total number of led used here */
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 14
+#define RGBLED_NUM 12
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
